683e5d7b7415758dcb00d7b4f96fa36f5dcd6378
Jarrod Miller <jarrod.miller@activeprospect.com>
2021-12-02 15:59:33 -0600

    Add Day 2 second solution

--- day_two.js
+++ day_two.js
@@ -21,11 +21,43 @@ const calculateDirections = function(directions) {
     inputDirection[direction](parseInt(distance));
   }); 
 
-  console.log(Math.abs(x * y));
+  console.log('CalculateDirections v1: ', Math.abs(x * y));
+};
+
+const calculateDirectionsImproved = function(directions) {
+  let x = 0;
+  let y = 0;
+  let aim = 0;
+
+  let inputDirection = {
+    forward: function(dist) {
+      x += dist
+      y += dist * aim
+    },
+    down: function(dist) {
+      aim += dist
+    },
+    up: function(dist) {
+      aim -= dist
+    }
+  };
+
+  directions.split('\n').forEach((line) => {
+    const [direction, distance] = line.split(' ');
+    inputDirection[direction](parseInt(distance));
+  }); 
+
+  console.log('CalculateDirections v2: ', Math.abs(x * y));
 };
 
 helper_elf.readFile('d2_inputs.csv')
   .then(calculateDirections)
+  .catch((error) => {
+    throw new Error(error);
+  })
+
+helper_elf.readFile('d2_inputs.csv')
+  .then(calculateDirectionsImproved)
   .catch((error) => {
     throw new Error(error);
   })
