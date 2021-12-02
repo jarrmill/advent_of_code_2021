c8e46685ca36b3d52ea0a287ed88f98d48bc61ba
Jarrod Miller <jarrod.miller@activeprospect.com>
2021-12-02 14:54:05 -0600

    Add day one pt. 2 solution

--- day_one.js
+++ day_one.js
@@ -1,24 +1,50 @@
 const helper_elf = require('./helper_elf');
 
-const check_depths = function (fileName, delimiter = '\n') {
-  helper_elf.readFile(fileName)
-    .then((data) => {
-      let counter = 0;
-      const depths = data.split(delimiter);
-      let lastDepthChecked = parseInt(depths.shift());
-      depths.forEach(depth => {
-        if (lastDepthChecked < parseInt(depth)) {
-          counter += 1;
-        };
+const check_depths = function(data) {
+  let counter = 0;
+  const depths = data.split('\n').map(depth => parseInt(depth));
+  let lastDepthChecked = depths.shift();
+  depths.forEach(depth => {
+    if (lastDepthChecked < depth) {
+      counter += 1;
+    };
 
-        lastDepthChecked = depth;
-      })
+    lastDepthChecked = depth;
+  })
 
-      console.log(counter);
-    })
-    .catch((error) => {
-      throw new Error(error);
-    })
+  console.log('Check depths v1: ', counter);
 }
 
-check_depths('d1_inputs.csv');
+const check_depths_improved = function(data) {
+  const getSum = (arr) => arr.reduce((a, b) => a + b);
+  const depths = data.split('\n').map(depth => parseInt(depth));
+  let counter = 0
+  let window = depths.splice(0, 3);
+  let lastDepthChecked = getSum(window);
+
+  depths.forEach(depth => {
+    window.shift();
+    window.push(depth);
+
+    const depthSum = getSum(window);
+    if (depthSum > lastDepthChecked) {
+      counter += 1;
+    }
+    lastDepthChecked = depthSum;
+  })
+
+  console.log('Check depths v2: ', counter);
+}
+
+
+helper_elf.readFile('d1_inputs.csv')
+  .then(check_depths)
+  .catch((error) => {
+    throw new Error(error);
+  })
+
+helper_elf.readFile('d1_inputs.csv')
+  .then(check_depths_improved)
+  .catch((error) => {
+    throw new Error(error);
+  })
